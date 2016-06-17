481966bd55f9766743abc2365f448abb128485da
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-06-17 22:47:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ elem = b.find_element_by_xpath('//span[contains(text(),"Saurabh G")]')
 elem.click()
 elem1 = b.find_elements_by_class_name('input')
 counter =0
-while counter < 5:
+while counter < 100:
     counter= counter +1
     elem1[1].send_keys('Your whatsapp is hacked')
     b.find_element_by_class_name('send-container').click()
