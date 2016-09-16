f573b6ab7ac8e252078134a8b2941d37e3cbd93e
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-17 00:08:02 +0530

    Add Comments

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 #!python3
 
+# To use this script first download selenuim through your terminal or command line
 from selenium import webdriver
+# Getting webdriver for firefox 
 b = webdriver.Firefox()
+# Requesting the web version of whatsapp
 b.get('http://web.whatsapp.com')
 input()
 elem = b.find_element_by_xpath('//span[contains(text(),"Friend's Name)]')
 elem.click()
 elem1 = b.find_elements_by_class_name('input')
 counter =0
+# Change the counter limit in while loop to send the message at your limit or times
 while counter < 100:
     counter= counter +1
+    # Change the message of your own choice
     elem1[1].send_keys('Your whatsapp is hacked')
     b.find_element_by_class_name('send-container').click()
